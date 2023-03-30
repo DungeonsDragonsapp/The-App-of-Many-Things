d4e483205b713710caea48f79f43bcab2cdc4eb3
Casey Regetz <nachochipman@gmail.com>
2023-03-30 17:23:58 -0400

    Login/Register Pages
    - Created Login and Register Pages
    - Allowed movement from login to register page, register to login, and login to main

---
+++ TheAppofManyThings/app/src/main/java/com/example/theappofmanythings/RegistrationActivity.kt
@@ -0,0 +1,22 @@
+package com.example.theappofmanythings
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_page)
+        val register = findViewById<View>(R.id.registerButton) as Button
+        register.movementMethod = LinkMovementMethod.getInstance()
+        register.setOnClickListener {
+            val intent = Intent(this@RegistrationActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
