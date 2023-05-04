ced126ccf108300fd830d0652d817e3b51d43cf7
LorenzoDitarantoNJIT <ld6@njit.edu>
2023-05-04 13:12:53 -0400

    image changes

--- TheAppofManyThings/app/src/main/java/com/example/theappofmanythings/CharacterCreationActivity.kt
+++ TheAppofManyThings/app/src/main/java/com/example/theappofmanythings/CharacterCreationActivity.kt
@@ -2,12 +2,19 @@ package com.example.theappofmanythings
 
 import android.R
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.parse.ParseObject
 import com.parse.ParseUser
+import java.io.IOException
 
 
 //public val character : ParseObject = ParseObject("character")
@@ -80,5 +87,56 @@ class CharacterCreationActivity : AppCompatActivity() {
     {
         const val TAG = "CharacterCreationActivity"
 
+    }
+
+    val PICK_PHOTO_CODE = 1046
+
+    // Trigger gallery selection for a photo
+    fun onPickPhoto(view: View?) {
+        // Create intent for picking a photo from the gallery
+        val intent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(packageManager) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE)
+        }
+    }
+
+    fun loadFromUri(photoUri: Uri?): Bitmap? {
+        var image: Bitmap? = null
+        try {
+            // check version of Android on device
+            image = if (Build.VERSION.SDK_INT > 27) {
+                // on newer versions of Android, use the new decodeBitmap method
+                val source: ImageDecoder.Source =
+                    ImageDecoder.createSource(this.contentResolver, photoUri)
+                ImageDecoder.decodeBitmap(source)
+            } else {
+                // support older versions of Android by using getBitmap
+                MediaStore.Images.Media.getBitmap(this.contentResolver, photoUri)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return image
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data != null && requestCode == PICK_PHOTO_CODE) {
+            val photoUri = data.data
+
+            // Load the image located at photoUri into selectedImage
+            val selectedImage = loadFromUri(photoUri)
+
+            // Load the selected image into a preview
+            val ivPreview: ImageView = findViewById<View>(com.example.theappofmanythings.R.id.imageView) as ImageView
+            ivPreview.setImageBitmap(selectedImage)
+        }
     }
 }
--- TheAppofManyThings/app/src/main/java/com/example/theappofmanythings/CharacterList.kt
+++ TheAppofManyThings/app/src/main/java/com/example/theappofmanythings/CharacterList.kt
@@ -44,52 +44,7 @@ class CharacterList : AppCompatActivity()  {
     val PICK_PHOTO_CODE = 1046
 
     // Trigger gallery selection for a photo
-    fun onPickPhoto(view: View?) {
-        // Create intent for picking a photo from the gallery
-        val intent = Intent(
-            Intent.ACTION_PICK,
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        )
 
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(packageManager) != null) {
-            // Bring up gallery to select a photo
-            startActivityForResult(intent, PICK_PHOTO_CODE)
-        }
-    }
-
-    fun loadFromUri(photoUri: Uri?): Bitmap? {
-        var image: Bitmap? = null
-        try {
-            // check version of Android on device
-            image = if (Build.VERSION.SDK_INT > 27) {
-                // on newer versions of Android, use the new decodeBitmap method
-                val source: ImageDecoder.Source =
-                    ImageDecoder.createSource(this.contentResolver, photoUri)
-                ImageDecoder.decodeBitmap(source)
-            } else {
-                // support older versions of Android by using getBitmap
-                MediaStore.Images.Media.getBitmap(this.contentResolver, photoUri)
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        return image
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (data != null && requestCode == PICK_PHOTO_CODE) {
-            val photoUri = data.data
-
-            // Load the image located at photoUri into selectedImage
-            val selectedImage = loadFromUri(photoUri)
-
-            // Load the selected image into a preview
-            val ivPreview: ImageView = findViewById<View>(R.id.ivPreview) as ImageView
-            ivPreview.setImageBitmap(selectedImage)
-        }
-    }
 
     private fun prepareCharacterRV() {
         //TODO get RV prepared
